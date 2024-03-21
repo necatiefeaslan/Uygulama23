e1a194b73e02b14de1563ac3fb0f2ba028ab8e1f
necati148 <necati148@RPC11>
2024-03-21 14:43:38 +0300

    Proje dosyası ekle.

---
+++ Uygulama23/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Uygulama23
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPlaka = new System.Windows.Forms.TextBox();
+            this.txtŞehir = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbSehirler = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plaka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şehir";
+            // 
+            // txtPlaka
+            // 
+            this.txtPlaka.Location = new System.Drawing.Point(132, 82);
+            this.txtPlaka.Name = "txtPlaka";
+            this.txtPlaka.Size = new System.Drawing.Size(38, 20);
+            this.txtPlaka.TabIndex = 2;
+            // 
+            // txtŞehir
+            // 
+            this.txtŞehir.Location = new System.Drawing.Point(132, 128);
+            this.txtŞehir.Name = "txtŞehir";
+            this.txtŞehir.Size = new System.Drawing.Size(100, 20);
+            this.txtŞehir.TabIndex = 3;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(156, 174);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Şehir Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(401, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Şehirler";
+            // 
+            // cbSehirler
+            // 
+            this.cbSehirler.FormattingEnabled = true;
+            this.cbSehirler.Location = new System.Drawing.Point(366, 117);
+            this.cbSehirler.Name = "cbSehirler";
+            this.cbSehirler.Size = new System.Drawing.Size(121, 21);
+            this.cbSehirler.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 405);
+            this.Controls.Add(this.cbSehirler);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtŞehir);
+            this.Controls.Add(this.txtPlaka);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPlaka;
+        private System.Windows.Forms.TextBox txtŞehir;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbSehirler;
+    }
+}
+
---
+++ Uygulama23/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama23
+{
+    public partial class Form1 : Form
+    {
+        List<Sehirler> listSehirler = new List<Sehirler>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Sehirler sehir = new Sehirler();
+            sehir.Plaka = txtPlaka.Text;
+            sehir.SehirAdi = txtŞehir.Text;
+            listSehirler.Add(sehir);
+            Bagla();
+        }
+        private void Bagla()
+        {
+            cbSehirler.DataSource = null;
+            cbSehirler.DataSource = listSehirler;
+            cbSehirler.DisplayMember = "SehirAdi";
+            cbSehirler.ValueMember = "Plaka";
+        }
+    }
+    
+    class Sehirler
+    {
+        public string Plaka { get; set; }
+        public string SehirAdi { get; set; }
+    }
+}
